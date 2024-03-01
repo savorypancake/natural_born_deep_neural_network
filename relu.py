78c9d41d49ffcea28b417179c8a335683a0d7939
Matthias Gumbert <matthias.gumbert@gmx.de>
2024-03-01 16:15:09 +0100

    added text description to specify function arguments and outputs.

--- relu.py
+++ relu.py
@@ -1,4 +1,18 @@
 def relu(Z):
+    """
+    Implement the RELU function.
+
+    Arguments:
+    Z -- Output of the linear layer, of any shape
+
+    Returns:
+    A -- Post-activation parameter, of the same shape as Z
+    cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
+    """
+
     A = max(0.0, Z)
+    assert(A.shape == Z.shape)
+
     cache = Z
+
     return A, cache
